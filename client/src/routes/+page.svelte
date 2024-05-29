d368957c81f29f0aae3cdf665b4c68867dc2dd06
Wo Meijer <79914012+WoMeijerPhD@users.noreply.github.com>
2024-05-29 14:32:56 +0200

    played around with mouse scrollwheel input

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script>
-    let video;
+    import {onMount} from 'svelte';
+   let video;
+   let videoTime = 0;
     let playButton;
     let pauseButton;
     let canvas;
+    let wheelTotal = 0;
 
     function playVideo() {
         video.play();
@@ -11,6 +14,98 @@
     function pauseVideo() {
         video.pause();
     }
+    let duration =0;
+    let lastDuration = 0;
+    const offset = .0001;
+    const numFullCanvases = 50;
+    let x = 0;
+    let y = 0;
+    let unboundY=0;
+    function drawCanvas(){
+        const ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.beginPath();
+        ctx.arc(x, y, 10, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+    function updateVideo(seekpercent){
+    video.currentTime = seekpercent * video.duration;
+    }
+   // on mount print hello world
+    onMount(() => {
+        video.addEventListener('timeupdate', () => {
+            videoTime = video.currentTime / video.duration;
+        });
+
+        document.addEventListener('wheel', (e) => {
+            console.log(e)
+            wheelTotal += e.deltaY;
+            e.preventDefault(); // Prevents page from scrolling
+        });
+
+
+        drawCanvas();
+        canvas.addEventListener("click", async () => {
+        if(!document.pointerLockElement) {
+            await canvas.requestPointerLock({
+            unadjustedMovement: true,
+            });
+        }
+        });
+
+
+        document.addEventListener("pointerlockchange", lockChangeAlert, false);
+
+        function lockChangeAlert() {
+        if (document.pointerLockElement === canvas) {
+            console.log("The pointer lock status is now locked");
+            document.addEventListener("mousemove", updatePosition, false);
+        } else {
+            console.log("The pointer lock status is now unlocked");
+            document.removeEventListener("mousemove", updatePosition, false);
+        }
+        }
+
+        let animation;
+        function updatePosition(e) {
+            // if canvas is undefined, return
+            if (!canvas) {
+                return;
+            }
+            function updateCoord(pos, delta, max) {
+                pos += delta;
+                pos %= max;
+                if (pos < 0) {
+                pos += max;
+                }
+                return pos;
+                }
+
+            x = updateCoord(x, e.movementX, canvas.width);
+            y = updateCoord(y, e.movementY, canvas.height);
+            unboundY += e.movementY;
+
+            duration = ((Math.abs(unboundY)/canvas.height)/numFullCanvases)%1;
+            // check if the difference between the last duration and the current duration is greater than the offset
+                if(Math.abs(duration - lastDuration) > offset) {
+                    console.log("changing duration");
+                    lastDuration = duration;
+                    updateVideo(duration);
+                }
+            
+            tracker.textContent = `X position: ${x}, Y position: ${y}, Duration: ${duration}`;
+
+            if (!animation) {
+                animation = requestAnimationFrame(function () {
+                animation = null;
+                drawCanvas();
+                });
+            }
+        }
+
+    });
+
+
 </script>
 
 <style>
@@ -58,7 +153,7 @@
 
 <div class="container">
     <div class="header">
-        <h1>Cyli Display test</h1>
+        <h1>Cyli Display test {wheelTotal}</h1>
     </div>
 
     <div class="overlay">
@@ -72,6 +167,7 @@
 
     <div class="footer">
         <div id="tracker"></div>
+        <input type="range" min="0" max="1" step="0.0001" bind:value={videoTime}>
         <button on:click={playVideo} bind:this={playButton}>Play</button>
         <button on:click={pauseVideo} bind:this={pauseButton}>Pause</button>
     </div>
